1744425e4f276e6652839686b7c930e70a60d07a
Lê Minh Hoàng <hoang.le.k3set@eiu.edu.vn>
2021-10-18 16:06:51 +0700

    first commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set;}
+    }
+}
