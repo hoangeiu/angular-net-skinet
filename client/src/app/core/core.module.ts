e75f85d797982a8b5aaef295b7c6ed90707112dc
Lê Minh Hoàng <hoang.le.k3set@eiu.edu.vn>
2021-10-26 11:49:35 +0700

    implement error interceptor http handling

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [SharedModule, RouterModule],
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+  ],
+  imports: [
+    SharedModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+  ],
   exports: [NavBarComponent],
 })
 export class CoreModule {}
