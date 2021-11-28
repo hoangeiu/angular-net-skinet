051beeb6bc3845e371dc2a49b6922dbb24e0da2d
Lê Minh Hoàng <hoang.le.k3set@eiu.edu.vn>
2021-11-28 08:40:50 +0700

    implement orders ui with direct html

---
+++ client/src/app/orders/orders.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrdersComponent } from './orders.component';
+import { OrdersRoutingModule } from './orders-routing.module';
+import { OrderDetailedComponent } from './order-detailed/order-detailed.component';
+
+@NgModule({
+  declarations: [OrdersComponent, OrderDetailedComponent],
+  imports: [CommonModule, OrdersRoutingModule],
+})
+export class OrdersModule {}
